1b1aa8bd38ea247e49a699f1223c4ab5a8bbef9f
pyxis <pyxis@uzuki.live>
2022-08-30 23:22:44 +0900

    git tag에 따라 docker image가 태그되도록 구현

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-docker build -t nexusstore.kohaku.moe/windsekirun/overtime-calculator .
-docker push nexusstore.kohaku.moe/windsekirun/overtime-calculator
+TAG=`git describe --exact-match --tags $(git log -n1 --pretty='%h')`
+echo Building nexusstore.kohaku.moe/windsekirun/overtime-calculator:$TAG
+docker build -t nexusstore.kohaku.moe/windsekirun/overtime-calculator:$TAG .
+docker push nexusstore.kohaku.moe/windsekirun/overtime-calculator:$TAG
